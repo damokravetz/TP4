336d1c6dbe224dd17123cae49201bcb1013e4a27
damokravetz <kravetzdamian@gmail.com>
2019-06-24 02:39:21 -0300

    vive

--- TP1-TP4-EXCEPCIONES/src/tp1/tp4/util/Test.java
+++ TP1-TP4-EXCEPCIONES/src/tp1/tp4/util/Test.java
@@ -9,7 +9,26 @@ public class Test {
 		int fallecimiento=0;
 		Scanner input = new Scanner(System.in);
 		LectorEnteros le = new LectorEnteros(input);
-		int anioActual;
+		int anioActual = Year.now().getValue();
+		
+		nacimiento=le.pedir("ingresar fecha de nacimiento de una persona (año)", 1900, anioActual);
+		RangoDeEnteros rango=new RangoDeEnteros(nacimiento, anioActual);
+		fallecimiento=le.pedir("ingresar año de fallecimiento o -1 si aun vive");
+		while(fallecimiento!=-1 && !rango.incluye(fallecimiento)) {
+			System.out.println("Fallecimiento no valido, ingrese nuevamente");
+			fallecimiento=le.pedir("ingresar año de fallecimiento o -1 si aun vive");
+		}
+		int edad=0;
+		if(fallecimiento!=-1) {
+			edad=fallecimiento-nacimiento;
+			System.out.println("Edad de la persona: "+edad+", no vive");
+		}else {
+			edad=anioActual-nacimiento;
+			System.out.println("Edad de la persona: "+edad", vive");
+		}
+		
+		
+
 		// Completar
 		
 		
